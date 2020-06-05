79d22ac9a20d179d43ebe4140e513b53210db5f4
Rachel Ruderman <raquel.rudermano@gmail.com>
2020-06-04 21:43:42 -0700

    Use destructuring to clean up

--- src/Counter/index.test.js
+++ src/Counter/index.test.js
@@ -3,9 +3,9 @@ import { render, cleanup } from 'react-testing-library';
 import Counter from './index';
 
 test('<Counter/>', () => {
-    const wrapper = render(<Counter/>);
-    wrapper.debug();
-    const element = wrapper.getByTestId('counter-button');
+    const { debug, getByTestId } = render(<Counter/>);
+    debug();
+    const element = getByTestId('counter-button');
     expect(element.tagName).toBe('BUTTON');
     expect(element.textContent).toBe('0');
 })
