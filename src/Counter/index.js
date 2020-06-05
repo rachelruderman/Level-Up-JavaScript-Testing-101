9d302bc947239b71f334d40894e3e555988854c1
Rachel Ruderman <raquel.rudermano@gmail.com>
2020-06-04 21:50:13 -0700

    Add onClick event to test

--- src/Counter/index.js
+++ src/Counter/index.js
@@ -6,11 +6,17 @@ export default class Counter extends Component {
         count: 0
     }
 
+    onClick = () => this.setState((prevState) => ({
+        count: prevState.count + 1
+    }));
+
     render() {
         const { count } = this.state;
         return (
             <div>
-                <button data-testid='counter-button'>{count}</button>
+                <button data-testid='counter-button' onClick={this.onClick}>
+                    {count}
+                </button>
             </div>
         )
     }
--- src/Counter/index.test.js
+++ src/Counter/index.test.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { render, cleanup } from 'react-testing-library';
+import { render, cleanup, fireEvent } from 'react-testing-library';
 import Counter from './index';
 
 test('<Counter/>', () => {
     const { debug, getByTestId } = render(<Counter/>);
     debug();
-    const element = getByTestId('counter-button');
-    expect(element.tagName).toBe('BUTTON');
-    expect(element.textContent).toBe('0');
+    const button = getByTestId('counter-button');
+    expect(button.tagName).toBe('BUTTON');
+    expect(button.textContent).toBe('0');
+
+    fireEvent.click(button);
+    expect(button.textContent).toBe('1');
+
 })
