deca1e02604f49e8d6483ca4a673841969bf2135
Rachel Ruderman <raquel.rudermano@gmail.com>
2020-06-04 21:25:41 -0700

    Boilerplate for testing in React

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,7 @@
-import { total } from './App';
-import { add } from './add'; // import a dependency
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-jest.mock('./add', () => ({ // mock the dependency (you don't want to test 3rd party APIs; you just want to test your code)
-    add: jest.fn(() => 3)
-}));
-
-test('total', () => {
-    const value = total(1, 2);
-    expect(value).toBe(`$3`);
-    expect(add).toHaveBeenCalledTimes(1);
-
-    add.mockImplementation(() => 30);
+test('renders without crashing', () => {
+  expect(true).toBeTruthy();
 });
--- src/add.test.js
+++
@@ -1,6 +0,0 @@
-import { add } from './add';
-
-test('add', () => {
-    const value = add(1, 2);
-    expect(value).toBe(3);
-});
