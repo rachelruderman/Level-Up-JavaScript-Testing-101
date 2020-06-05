282ed0c1a36ba8f667744e6b2d49578675b8350c
Rachel Ruderman <raquel.rudermano@gmail.com>
2020-06-04 21:19:50 -0700

    Mock dependency in test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,14 @@
-const add = jest.fn(() => 3);
+import { total } from './App';
+import { add } from './add'; // import a dependency
 
-test('add', () => {
-    const value = add(1, 2);
-    expect(value).toBe(3);
+jest.mock('./add', () => ({ // mock the dependency (you don't want to test 3rd party APIs; you just want to test your code)
+    add: jest.fn(() => 3)
+}));
+
+test('total', () => {
+    const value = total(1, 2);
+    expect(value).toBe(`$3`);
     expect(add).toHaveBeenCalledTimes(1);
+
+    add.mockImplementation(() => 30);
 });
---
+++ src/add.test.js
@@ -0,0 +1,6 @@
+import { add } from './add';
+
+test('add', () => {
+    const value = add(1, 2);
+    expect(value).toBe(3);
+});
